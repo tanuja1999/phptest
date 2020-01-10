5da9207e3912e7d69a56365122622d520dae7821
Tanuja Mandiye <tanuja876mandiye@gmail.com>
2020-01-10 11:44:55 +0530

    projects

---
+++ localvar.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+function myTest()
+{
+    $x = 5; // local scope
+    echo "<p> Variable x inside function is : $x </p>";
+}
+myTest();
+// using x outside the function will generate an error
+echo "<p>Variable x outside function is: $x</p>";
+?>
+</body>
+</html>
